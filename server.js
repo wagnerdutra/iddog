9d2f078b8387f7f0946346ceb8a3cd6a82c0c750
wagner.dutra <wagnerdutra1010@gmail.com>
2019-07-19 12:50:16 -0300

    serverjs

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(path.join(__dirname, '/build/favicon.ico')));
+app.use(favicon(path.join(__dirname, '/dist/favicon.ico')));
 // the __dirname is the current directory from where the script is running
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
